64722ecc07fe6dccae16b6edfea9ed232e4d1d69
670600971 <670600971@qq.com>
2019-04-28 15:53:33 +0800

    上传头像

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -236,6 +236,53 @@ class User extends Base
     }
 
     /**
+     * 上传头像
+     */
+    public function upload_avatar()
+    {
+        $base64 = input('post.dataImg');
+        $user_id = $this->user_id;
+        $res = $this->uploadImg($base64, $user_id);
+        return $res;
+    }
+
+    /**
+     * 处理base64
+    */
+    function uploadImg($base64,$user_id){
+        header("content-type:text/html;charset=utf-8");
+        $base64_image = str_replace(' ', '+', $base64);
+        //post的数据里面，加号会被替换为空格，需要重新替换回来，如果不是post的数据，则注释掉这一行
+        if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $base64_image, $result)){
+            //匹配成功
+            if($result[2] == 'jpeg'){
+                $image_name = uniqid().'.jpg';
+                //纯粹是看jpeg不爽才替换的
+            }else{
+                $image_name = uniqid().'.'.$result[2];
+            }
+            $image_file = "./uploads/".date('Ymd',time()).'/';
+            if (!file_exists($image_file)) {
+
+                mkdir($image_file,0755,true);
+            }
+            $image_url = "./uploads/".date('Ymd',time()).'/'."{$image_name}";
+            $res_url = "/uploads/".date('Ymd',time()).'/'."{$image_name}";
+            $res = Db::name('users')->where('id',$user_id)->update(['avatar' => $res_url]);
+            $user = Db::name('users')->where('id', $user_id)->find();
+            Session::set('user', $user);
+            //服务器文件存储路径
+            if ($res && file_put_contents($image_url, base64_decode(str_replace($result[1], '', $base64_image)))){
+                return json(['code'=>200, 'msg'=>'上传成功', 'imgUrl'=>$res_url]);
+            }else{
+                return json(['code'=>0, 'msg'=>'上传失败']);
+
+            }
+        }else{
+            return json(['code'=>0, 'msg'=>'上传失败..']);
+        }
+    }
+    /**
      *检查用户是否登陆
      */      
     public function check_user(){
