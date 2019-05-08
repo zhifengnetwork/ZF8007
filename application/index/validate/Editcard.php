dbf2766a4fd8842784c2fa6304ed1b58ad8bc5ac
670600971 <670600971@qq.com>
2019-05-08 18:58:19 +0800

    提交

--- application/index/validate/Editcard.php
+++ application/index/validate/Editcard.php
@@ -16,18 +16,18 @@ class Editcard extends validate
     ];
     protected $message = [
         
-        'value.require'           => '请填写手机号',
+        'value.require'           => '请填写银行名称',
         'value.chsAlpha'          => '银行名称必须是汉字或者字母',
         'user_name.require'       => '请填写姓名',
         'user_name.chsAlpha'      => '姓名必须是数字或者字母',
         'account.alphaNum'        => '请填写数字或者字母',
         'address.chsAlphaNum'          => '地址是汉字字母或者数字',
-        'acount.require'          => '请填写账号',
+        'account.require'          => '请填写账号',
         
     ];
     protected $scene = [
-        'editcard' => [ 'value', 'username','account','address'],
-        'pay' => [ 'username', 'account'],
+        'editcard' => [ 'value', 'user_name','account','address'],
+        'pay' => [ 'account','user_name', ],
         // 'del' => ['admin_id'],
     ];
 
