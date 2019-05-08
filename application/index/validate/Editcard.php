db6d0139153271446fce28b29e5431d010b84327
670600971 <670600971@qq.com>
2019-05-08 20:03:01 +0800

    验证

--- application/index/validate/Editcard.php
+++ application/index/validate/Editcard.php
@@ -8,21 +8,24 @@ class Editcard extends validate
 {
 
     protected $rule = [
-        'value'         => 'require|chsAlpha',
+        'value'         => 'require|chsAlpha|max:30',
         'user_name'     => 'require|chsAlpha',
-        'account'       =>  'require|alphaNum', 
+        'account'       =>  'require|alphaNum|length:16,19', 
         'address'       => 'chsAlphaNum',
+        
         
     ];
     protected $message = [
         
         'value.require'           => '请填写银行名称',
         'value.chsAlpha'          => '银行名称必须是汉字或者字母',
+        'value.max'               => '银行名称最长30位',
         'user_name.require'       => '请填写姓名',
         'user_name.chsAlpha'      => '姓名必须是数字或者字母',
         'account.alphaNum'        => '请填写数字或者字母',
-        'address.chsAlphaNum'          => '地址是汉字字母或者数字',
-        'account.require'          => '请填写账号',
+        'address.chsAlphaNum'     => '地址是汉字字母或者数字',
+        'account.require'         => '请填写卡号',
+        'account.length'          => '银行卡号长度16-19位',
         
     ];
     // protected $scene = [
--- application/index/validate/Pay.php
+++ application/index/validate/Pay.php
@@ -8,15 +8,17 @@ class Pay extends validate
 {
 
     protected $rule = [
-        'account'       =>  'require|alphaNum',
-        'user_name'     => 'require|chsAlpha',
+        'account'       =>  'require|alphaNum|max:25',
+        'user_name'     => 'require|chsAlpha|max：30',
     ];
     protected $message = [
         
         'user_name.require'       => '请填写姓名',
+        'user_name.max'           => '姓名最大长度30位',
         'user_name.chsAlpha'      => '姓名必须是数字或者字母',
         'account.alphaNum'        => '请填写数字或者字母',
-        'account.require'          => '请填写账号',
+        'account.require'         => '请填写账号',
+        'account.max'             => '账号长度最大25位' 
         
     ];
 
