4dda6228c83a9e460cccaf8a85c60e55bf81dd11
BlizzardWu <2442668602@qq.com>
2019-05-22 10:15:13 +0800

    前端：返回

--- public/static/mobile/js/public.js
+++ public/static/mobile/js/public.js
@@ -1,9 +1,15 @@
+/* 返回只执行一次 */
+var state = true;
 /*返回 按钮*/
 function returnFun(){
+	if(!state){
+		return false;
+	}
 	/*返回上一页*/
 	if($('.lb_headWrap .lb_headWrap_return').attr('data-num') == 1 || $('.headWrap_lb .returnBut_lb').attr('data-num') == undefined ){
 		window.history.back();
 		console.log("返回上一页");
+		state = false;
 	}else {
 		/*页面跳转*/
 		window.location.href = $('.lb_headWrap .lb_headWrap_return').attr('data-num');
